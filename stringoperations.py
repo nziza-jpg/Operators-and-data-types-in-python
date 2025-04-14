9dd87935171388a67ecd3f8bf6176a6dd7637d5e
Nziza.jpg <tayalinziza@gmail.com>
2025-04-14 18:56:02 +0200

    done

---
+++ stringoperations.py
@@ -0,0 +1,13 @@
+first_name = "Nziza"
+last_name = "Tayali"
+full_name = first_name+last_name
+example = "Haa"*5
+print("First name :", first_name)
+print("Last name :", last_name)
+print("Full Name :", full_name)
+print("String multiplied 5 times gives this result :", example)
+word = 'Coding'
+print("Length of string :", len(word))
+print("First letter of string :", word[0])
+print("Last letter of string :", word[5])
+print("String sliced :", word[0:3]) 

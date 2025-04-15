8ae8be4f0f3998278b14a78d5ab364e1f4e93082
Nziza.jpg <tayalinziza@gmail.com>
2025-04-15 15:46:13 +0200

    done

---
+++ project.py
@@ -0,0 +1,11 @@
+x = input("Enter Value of x:")
+y = input("Enter Value of y:")
+z = input("Enter Value of z:")
+temp = x
+x = z
+z = temp
+z = y
+y = temp
+print("value of x after swapping", x)
+print("value of y after swapping", y)
+print("value of z after swapping", z)
